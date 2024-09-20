23b6e17074453c754078740b5466c2b0e2d89e51
Ritam <ritamdevs2004@gmail.com>
2024-09-20 22:33:29 +0530

    first_init/nav

---
+++ src/Utils/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useWindowSize = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return isLargeScreen;
+};
+
